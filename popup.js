1f1d07b62aaab601274056d8685070c5021c7bff
chrisnnchris <chrisnnchris@gmail.com>
2018-04-20 15:26:21 -0700

    Clean up

--- popup.js
+++ popup.js
@@ -1,27 +1,37 @@
-
 document.addEventListener('DOMContentLoaded', () => {
 
 
 
   console.log("This is the page verifier " + onSubscriptionPage.verifyPage());
   if (authYoutubeToPocket.isAuthenticated() === true) {
-    $('#pocketAuthenticationVerifier').text('Authentication worked');
+    $('#pocketAuthenticationVerifier').text('You are authenticated and can add');
   }
   document.getElementById("authenticateButton").addEventListener('click',  () => {
-      console.log("This is the isAuthenticated result")
-      console.log(!authYoutubeToPocket.isAuthenticated());
+      //console.log("This is the isAuthenticated result")
+      //console.log(!authYoutubeToPocket.isAuthenticated());
+    if (authYoutubeToPocket.isAuthenticated() === false) {
       authYoutubeToPocket.authenticate();
-
+    } else {
+      console.log("you are already authenticated")
+    }
   });
   document.getElementById("clearTokenButton").addEventListener('click', () => {
-    authYoutubeToPocket.logout();
+    if (authYoutubeToPocket.isAuthenticated() === true) {
+      authYoutubeToPocket.logout();
+    } else {
+      console.log("You aren't authenticated already");
+    }
   });
   document.getElementById("YoutubeScrapeToPocket").addEventListener('click', () => {
-    chrome.tabs.executeScript({
-      file: "extract.js"
-    }, function (result) {
-      addToPocket.YoutubeSubsToPocket(result);
-    });
+    if (authYoutubeToPocket.isAuthenticated() === true) {
+      chrome.tabs.executeScript({
+        file: "extract.js"
+      }, function (result) {
+        addToPocket.YoutubeSubsToPocket(result);
+      });
+    } else {
+      console.log("Can't add. Not authenticated")
+    }
   });
 
   console.log("This log message just goes to popup console, put in a chrome.tabs to get real browser");
